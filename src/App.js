723d48b8fd199168a21b66c0ca4063dcc2828ab1
Shreya Saxena <shreyasaxena066@gmail.com>
2018-12-26 01:44:14 +0530

    Initializing the Contact Manager app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+	<Header branding="Contact Manager"/>
+  <div className=" container">
+  <Contact name="JOhn DOe"
+      email="sfdjg@gmail.com"
+      phone="7835"/>
+      
+      <Contact name="shreya"
+      email="sfdeejg@gmail.com"
+      phone="7dfg5"/>
+  
+  </div>
+      
+</div>
+    );
+  }
+}
+
+export default App;
